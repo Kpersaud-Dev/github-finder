9683d06cf297bd3de8ba3b248e4ed266a9378e32
kevin_p <kprsd14@gmail.com>
2020-09-17 17:44:30 -0400

    Added async await

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Init GitHub
+const github = new Github;
+// Init UI
+const ui = new UI;
+
+
 // Search input
 const searchUser = document.getElementById('searchUser');
 
@@ -7,6 +13,20 @@ searchUser.addEventListener('keyup', (e) => {
   const userText = e.target.value;
 
   if (userText !== '') {
-    console.log(userText);
+    // Make http call
+    github.getUser(userText)
+      .then(data => {
+        if (data.profile.message === 'Not Found') {
+          // Show alert
+          ui.showAlert('User not found', 'alert alert-danger');
+        } else {
+          // Show Profile
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+        }
+      })
+  } else {
+    // Clear profile
+    ui.clearProfile();
   }
-})
+});
---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'a5bd3e7002e7f367d6ae';
+    this.client_secret = '8c408b5d7981dcfbeb3eeffe580afa211bf06e3b';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_count_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
